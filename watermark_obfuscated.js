c94a4a31f7cee48c1c832e8a4e7bcb467b334581
CriminalYT <155567350+CriminalYT@users.noreply.github.com>
2024-02-17 17:56:08 +0100

    Create watermark_obfuscated.js

---
+++ watermark_obfuscated.js
@@ -0,0 +1,38 @@
+(function() {
+    'use strict';
+
+    // Watermark texts
+    var watermark1 = `Criminal Squad <span class="user">${user}</span>`; // Updated watermark text
+
+    // Style for the watermark
+    var style = document.createElement('style');
+    style.innerHTML = `
+        .watermark {
+            position: fixed;
+            top: 10px;
+            left: 10px;
+            font-family: 'Arial', sans-serif;
+            font-size: 14px;
+            padding: 3px 5px;
+            background-color: #221f22;
+            color: #005aff;
+            border-radius: 3px;
+            z-index: 9999;
+            pointer-events: none;
+            box-shadow: 0 0 3px 2px #005aff;
+        }
+        .user {
+            color: #fff !important;
+        }
+    `;
+
+    document.head.appendChild(style);
+
+    // Create watermark element
+    var watermark = document.createElement('div');
+    watermark.className = 'watermark';
+    watermark.innerHTML = `${watermark1}`;
+
+    // Add watermark to the page
+    document.body.appendChild(watermark);
+})();
